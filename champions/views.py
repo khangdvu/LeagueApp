db63ce4b24739fda28c9d5158a971d91d6d34afd
Khang Vu <khang.dinh.vu@gmail.com>
2021-07-09 15:21:00 -0700

    Modify serializer to work with frontend

--- champions/views.py
+++ champions/views.py
@@ -4,15 +4,7 @@ from .models import Champion
 from rest_framework import viewsets
 from .serializers import ChampionSerializer
 
-""" def index(request):
-    all_champions = Champion.objects.all()
-    template = loader.get_template('champions/champions.html')
-    context = {
-        'all_champions' : all_champions,
-    }
-    return HttpResponse(template.render(context, request))
- """
 
 class ChampionViewSet(viewsets.ModelViewSet):
-    queryset = Champion.objects.all().order_by('hp')
+    queryset = Champion.objects.all().order_by('name')
     serializer_class = ChampionSerializer
